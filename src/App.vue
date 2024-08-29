42bf1f262c1f29949e712f4f189a897f854e77ba
YousifVN <yousifmahmood995@gmail.com>
2024-08-29 05:26:08 +0300

    added scroll to tob from naiveui

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@ import { RouterView } from 'vue-router'
 
 import Sidebar from './components/layout/Sidebar.vue';
 import SearchBox from './components/layout/SearchBox.vue';
-
-import Button from './components/common/Button.vue';
 import PageFooter from './components/layout/PageFooter.vue';
+import Button from './components/common/Button.vue';
+
+// import { NBackTop } from 'naive-ui'
+
 </script>
 
 <template>
@@ -14,12 +16,14 @@ import PageFooter from './components/layout/PageFooter.vue';
             <Sidebar class="w-[80px] relative sm:mr-[200px]" />
             <SearchBox class="flex-grow mt-3" />
             <div><Button title="Log In" icon="<i class='fa-solid fa-user'></i>" class="mt-3 mr-5" /></div>
-            
+
         </div>
         <div class="text-white sm:ml-[260px]">
             <RouterView />
         </div>
 
-        <PageFooter class="sm:ml-[260px]"/>
+        <!-- <n-back-top :right="80" /> -->
+
+        <PageFooter class="sm:ml-[260px]" />
     </div>
 </template>
